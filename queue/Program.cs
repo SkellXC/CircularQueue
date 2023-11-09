307801605f1eacbd6a9c3ddb8ba89cb15c671d2a
Ethan Travasso <231208@student.wqe.ac.uk>
2023-11-09 15:56:47 +0000

    Commented stuff

--- queue/Program.cs
+++ queue/Program.cs
@@ -9,7 +9,7 @@ namespace queue
     {
         static void Main(string[] args)
         {
-            string[] queue = new string[3];
+            string[] queue = new string[3];// Creates a list with length 3
             int front = -1;
             int back = -1;
             int size = 0;
@@ -35,28 +35,26 @@ namespace queue
         {   // If the queue is not full
             if (!isFull(queue, size))
             {
-                int max = 3;
+                const int max = 3; // uses max for modulo to make it clear why 'back' is being divided by a number
                 size += 1;
                 back = (back + 1) % max;// Sends the position back to 1 if it goes past the end of the list
                 queue[back] = item;
                 Console.WriteLine(back);
                 return;
-
             }
-            Console.WriteLine("Queue is full");
+            Console.WriteLine("Queue is full. Cannot add anything until a spot is available");
         }
         static void deQueue(string[] queue, ref int size, ref int front)
         {
             if (!isEmpty(queue, size))
             {
-                int max = 3;
+                const int max = 3; // Check isfull
                 size -= 1;
-                front = (front + 1) % max;
-                queue[front] = null;
-                
-
+                front = (front + 1) % max; // Sends the position back to 1 if it goes past the end of the list
+                queue[front] = null;// Overwrites the item in the front and sets it to null
+                return;
             }
-
+            Console.WriteLine("Queue is Empty. Cannot dequeue anything");
         }
         static void DisplayList(string[] queue, int size, int front)
         {
